05b11103a17c0a1da4981e1020f01a411ed04ad6
bob <mangoraft@gmail.com>
2015-03-03 15:04:51 -0500

    added sideband on push

--- example/simple.js
+++ example/simple.js
@@ -1,20 +1,23 @@
 var pushover = require('../');
 var repos = pushover('/tmp/repos');
 
-repos.on('push', function (push) {
-    console.log('push ' + push.repo + '/' + push.commit
-        + ' (' + push.branch + ')'
-    );
-    push.accept();
+repos.on('push', function(push) {
+	console.log('push ' + push.repo + '/' + push.commit + ' (' + push.branch + ')');
+	push.accept();
+	push.once('sideband', function(sideband) {
+		sideband.write('sadasdasdsdad\n');
+		sideband.end('sadasdasdsdad\n');
+	});
+
 });
 
-repos.on('fetch', function (fetch) {
-    console.log('fetch ' + fetch.repo + '/' + fetch.commit);
-    fetch.accept();
+repos.on('fetch', function(fetch) {
+	console.log('fetch ' + fetch.repo + '/' + fetch.commit);
+	fetch.accept();
 });
 
 var http = require('http');
-var server = http.createServer(function (req, res) {
-    repos.handle(req, res);
+var server = http.createServer(function(req, res) {
+	repos.handle(req, res);
 });
 server.listen(7005);
